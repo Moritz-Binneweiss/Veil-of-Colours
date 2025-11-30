68a331cea5899d9ca9c2a38f202a28f480d837b7
Neassine <nessaschoger@gmail.com>
2025-11-30 19:42:34 +0100

    Redo Level A + Item Script

--- Veil-of-Colours/Assets/Scripts/Item/ItemManager.cs
+++ Veil-of-Colours/Assets/Scripts/Item/ItemManager.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class ItemManager : MonoBehaviour
 {
+   
+   [Header("Key Settings")]
+    public string keyID = "KeyA"; // Für Keys: welcher Key wird gesammelt?
+    public string requiredKeyID = "KeyA"; // Für Doors: welcher Key wird benötigt?
+
+
+    [SerializeField]
+    public UnityEvent OnUse;
+
+    private static Dictionary<string, bool> playerKeys = new Dictionary<string, bool>();
+
+     public static ItemManager Instance { get; private set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,13 +24,59 @@ public class ItemManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+            // Initialize keys wenn noch nicht vorhanden
+            if (playerKeys.Count == 0)
+            {
+                playerKeys["KeyA"] = false;
+                playerKeys["KeyB"] = false;
+                playerKeys["KeyC"] = false; // Für weitere Keys
+            }
+        }
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            UseItem();
+            
+        }
+
+    }
+
+    public void UseItem()
+    {
+        OnUse.Invoke();
+    }
+
+    public void CollectKey()
+    {
+         if (playerKeys.ContainsKey(keyID))
+        {
+            playerKeys[keyID] = true;
+            Debug.Log($"Key {keyID} collected!");
+        Destroy(gameObject);
+        }
+        
         
     }
 
-    public void CollectItem()
+    public void UseKey()
     {
-        
+           if (playerKeys.ContainsKey(requiredKeyID) && playerKeys[requiredKeyID])
+        {
+            playerKeys[requiredKeyID] = false; // Key verbrauchen
+            Debug.Log($"Door opened with {requiredKeyID}!");
+            Destroy(gameObject); // Door zerstören
+        }
     }
 }
