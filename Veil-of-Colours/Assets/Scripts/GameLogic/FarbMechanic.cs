a132113fff74c5a819dbde6ea21e5611c6e26ef3
Neassine <nessaschoger@gmail.com>
2025-12-02 16:02:09 +0100

    Add third color via Networking

--- Veil-of-Colours/Assets/Scripts/GameLogic/FarbMechanic.cs
+++ Veil-of-Colours/Assets/Scripts/GameLogic/FarbMechanic.cs
@@ -1,30 +1,49 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Netcode;
 using UnityEngine.Tilemaps;
 
-public class FarbMechanic: MonoBehaviour
+public class FarbMechanic : NetworkBehaviour
 {
-     [Header("Farblayer GameObjects")]
+    [Header("Farblayer GameObjects")]
     [SerializeField]
     private Tilemap Farblayer1;
     
     [SerializeField]
     private Tilemap Farblayer2;
 
-     [Header("Input Actions")]
-        [SerializeField]
-        private InputActionReference Color1;
+     [SerializeField]
+    private Tilemap Farblayer3;
 
-        [SerializeField]
-        private InputActionReference Color2;
+    [Header("Input Actions")]
+    [SerializeField]
+    private InputActionReference Color1;
 
+    [SerializeField]
+    private InputActionReference Color2;
+
+     [SerializeField]
+    private InputActionReference Color3;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
-        ReadInput(); 
+        // Nur der Owner kann Input senden
+        if (!IsOwner) return;
+        
+        ReadInput();
     }
 
-     private void ReadInput()
+    public void switchFarblayer()
+    {
+        
+    }
+
+    private void ReadInput()
     {
         // Prüfe Input für Color1 (Taste 1)
         if (Color1.action.WasPressedThisFrame())
@@ -37,48 +56,71 @@ public class FarbMechanic: MonoBehaviour
         {
             ActivateFarblayer2();
         }
+
+         if (Color3.action.WasPressedThisFrame())
+        {
+            ToggleFarblayer3ForOtherPlayerServerRpc();
+        }
     }
+     [ServerRpc]
+    private void ToggleFarblayer3ForOtherPlayerServerRpc(ServerRpcParams rpcParams = default)
+    {
+        // An alle anderen Clients senden (außer Sender)
+        ulong senderClientId = rpcParams.Receive.SenderClientId;
+        
+        // ClientRpc an alle außer Sender
+        ToggleFarblayer3ClientRpc(senderClientId);
+        
+        Debug.Log($"Player {senderClientId} triggered Farblayer3 toggle for other players");
+    }
+
+    [ClientRpc]
+    private void ToggleFarblayer3ClientRpc(ulong senderClientId)
+    {
+        // Nur ausführen wenn nicht der Sender
+        if (NetworkManager.Singleton.LocalClientId == senderClientId) return;
+        
+        if (Farblayer3 != null)
+        {
+            bool isActive = Farblayer3.gameObject.activeSelf;
+            Farblayer3.gameObject.SetActive(!isActive);
+            Debug.Log($"Farblayer3 {(!isActive ? "activated" : "deactivated")} by Player {senderClientId}!");
+        }
+    }
+
 
     private void ActivateFarblayer1()
     {
-        if (Farblayer1 != null)
+        if (Farblayer1 != null && Farblayer2 != null)
         {
-            Farblayer1.gameObject.SetActive(false);
-            Debug.Log("Farblayer1 activated!");
-            
-            // Optional: Farblayer2 deaktivieren
-            if (Farblayer2 != null)
-            {
-                Farblayer2.gameObject.SetActive(true);
-            }
+            Farblayer1.gameObject.SetActive(true);
+            Farblayer2.gameObject.SetActive(false);
+            Debug.Log("Farblayer1 activated, Farblayer2 deactivated!");
         }
     }
 
     private void ActivateFarblayer2()
     {
-        if (Farblayer2 != null)
+        if (Farblayer1 != null && Farblayer2 != null)
         {
-            Farblayer2.gameObject.SetActive(false);
-            Debug.Log("Farblayer2 activated!");
-            
-            // Optional: Farblayer1 deaktivieren
-            if (Farblayer1 != null)
-            {
-                Farblayer1.gameObject.SetActive(true);
-            }
+            Farblayer1.gameObject.SetActive(false);
+            Farblayer2.gameObject.SetActive(true);
+            Debug.Log("Farblayer2 activated, Farblayer1 deactivated!");
         }
     }
-    
 
-     void OnEnable()
+   
+    void OnEnable()
     {
         Color1.action.Enable();
         Color2.action.Enable();
+        Color3.action.Enable();
     }
 
     void OnDisable()
     {
         Color1.action.Disable();
         Color2.action.Disable();
+        Color3.action.Disable();
     }
 }
