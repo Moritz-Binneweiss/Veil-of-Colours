8d4b818a12b26ecc8eba39756fb7e021ba6e9250
Neassine <nessaschoger@gmail.com>
2026-01-07 15:07:27 +0100

    added light to checkpoint

--- Veil-of-Colours/Assets/Scripts/GameLogic/CheckpointScript.cs
+++ Veil-of-Colours/Assets/Scripts/GameLogic/CheckpointScript.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CheckpointScript : MonoBehaviour
 {
     public Sprite activatedCheckpointSprite;
+    public Light2D checkpointLight;
     private bool activated = false;
 
     private void Reset() => GetComponent<Collider2D>().isTrigger = true;
@@ -26,6 +28,8 @@ public class CheckpointScript : MonoBehaviour
         if (spriteRenderer != null && activatedCheckpointSprite != null)
         {
             spriteRenderer.sprite = isActive ? activatedCheckpointSprite : spriteRenderer.sprite;
+            checkpointLight.enabled = isActive;
+            GetComponent<Collider2D>().enabled = false;
         }
     }
 }
